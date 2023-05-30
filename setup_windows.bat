76fca36763f2aea1434618cd41159d24f3665281
John Smith <nobody@example.com>
2023-05-29 17:04:28 -0700

    fixes for windows

--- setup_windows.bat
+++ setup_windows.bat
@@ -14,15 +14,15 @@ IF NOT DEFINED ProgramFiles(x86) (
 
 FOR %%X IN (protoc.exe) DO (SET PROTOC_FOUND=%%~$PATH:X)
 IF NOT DEFINED PROTOC_FOUND (
-    echo protobuf compiler ^(protoc^) is required but it's not installed. Install protoc 21.10 or higher. Ensure it is in your path. Aborting.
-    echo protoc is available here: https://github.com/protocolbuffers/protobuf/releases/download/v21.10/protoc-21.10-win64.zip
+    echo protobuf compiler ^(protoc^) is required but it's not installed. Install protoc 23.2 or higher. Ensure it is in your path. Aborting.
+    echo protoc is available here: https://github.com/protocolbuffers/protobuf/releases/download/v23.2/protoc-23.2-win64.zip
     goto end
 )
 
 FOR %%X IN (capnp.exe) DO (SET CAPNP_FOUND=%%~$PATH:X)
 IF NOT DEFINED CAPNP_FOUND (
-    echo capnproto compiler ^(capnp^) is required but it's not installed. Install capnp 0.10.3 or higher. Ensure it is in your path. Aborting.
-    echo capnp is available here: https://capnproto.org/capnproto-c++-win32-0.10.3.zip
+    echo capnproto compiler ^(capnp^) is required but it's not installed. Install capnp 0.10.4 or higher. Ensure it is in your path. Aborting.
+    echo capnp is available here: https://capnproto.org/capnproto-c++-win32-0.10.4.zip
     goto end
 )
 
