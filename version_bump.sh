dbd61a92555e679d76a1812cd772e675f09c81e0
Christien Rioux <chris@veilid.org>
2023-07-16 14:51:12 -0400

    Versions

--- version_bump.sh
+++ version_bump.sh
@@ -3,11 +3,19 @@ set -e
 
 if [ "$1" == "patch" ]; then
     echo Bumping patch version
+    PART=patch
 elif [ "$1" == "minor" ]; then
     echo Bumping minor version
+    PART=minor
 elif [ "$1" == "major" ]; then
     echo Bumping major version
+    PART=major
+else
+    echo Unsupported part! Specify 'patch', 'minor', or 'major'
+    exit 1
 fi
 
-cargo set-version --dry-run --bump $1
+CARGO_TOMLS=$(find . -name Cargo.toml -not -path ./external/\* -not -path ./Cargo.toml | xargs)
+
+bumpversion --dry-run $PART $CARGO_TOMLS ./veilid-python/pyproject.toml ./veilid-flutter/pubspec.yaml
 
