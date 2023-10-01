2dbd793878a977491207b7dd73d962c2be60de78
Mike Phipps <15438609-k8wu@users.noreply.gitlab.com>
2023-10-01 15:57:58 -0400

    The wasm_build.sh script is now more resilient to failures in finding llvm-dwarfdump

--- veilid-wasm/wasm_build.sh
+++ veilid-wasm/wasm_build.sh
@@ -14,13 +14,13 @@ if [ -f /usr/local/opt/llvm/bin/llvm-dwarfdump ]; then
     DWARFDUMP=/usr/local/opt/llvm/bin/llvm-dwarfdump
 elif [ -f /opt/homebrew/llvm/bin/llvm-dwarfdump ]; then
     DWARFDUMP=/opt/homebrew/llvm/bin/llvm-dwarfdump
-else 
-    DWARFDUMP=`which llvm-dwarfdump`
+else
+    # some systems may have the major LLVM version suffixed on the LLVM binaries - and we need `true` at the end because the whole script will fail with a nonzero return if something goes wrong here
+    DWARFDUMP=`which llvm-dwarfdump || find ${PATH//:/\/ } -name 'llvm-dwarfdump*' 2>/dev/null | head -n1 || true`
     if [[ "${DWARFDUMP}" == "" ]]; then
-        echo llvm-dwarfdump not found
+        echo "llvm-dwarfdump not found"
     fi
 fi
-
 
 if [[ "$1" == "release" ]]; then  
     OUTPUTDIR=../target/wasm32-unknown-unknown/release/pkg
