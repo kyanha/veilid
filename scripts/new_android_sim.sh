5976d30832a2a48f2d23527efea4a766bc44759c
Christien Rioux <chris@veilid.org>
2024-06-02 18:15:21 -0400

    revert fix

--- scripts/new_android_sim.sh
+++ scripts/new_android_sim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-UNAME_M=`uname -m`
+UNAME_M=${1-$(uname -m)}
 if [[ "$UNAME_M" == "arm64" ]]; then
     ANDROID_ABI=arm64-v8a
 elif [[ "$UNAME_M" == "x86_64" ]]; then
@@ -14,16 +14,13 @@ AVD_TAG="google_atd"
 AVD_IMAGE="system-images;android-30;$AVD_TAG;$ANDROID_ABI"
 AVD_DEVICE="Nexus 10"
 
-SDKMANAGER=$ANDROID_HOME/tools/bin/sdkmanager
-AVDMANAGER=$ANDROID_HOME/tools/bin/avdmanager
+SDKMANAGER=$ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager
+AVDMANAGER=$ANDROID_HOME/cmdline-tools/latest/bin/avdmanager
 if ! command -v $SDKMANAGER; then
-    SDKMANAGER=$ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager
-    AVDMANAGER=$ANDROID_HOME/cmdline-tools/latest/bin/avdmanager
-    if ! command -v $SDKMANAGER; then
-        echo "Can't find 'sdkmanager' in the usual places."
-        exit
-    fi
+    echo "Can't find 'sdkmanager' in the usual places."
+    exit
 fi
+
 EMULATOR=$ANDROID_HOME/emulator/emulator
 if ! command -v $EMULATOR; then
     echo "Can't find 'emulator' in the usual places."
